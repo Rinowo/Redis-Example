c4b85110668deff47d1a7f4d4762d0e4fc8855a2
Rinowo <tunganhhh205@gmail.com>
2023-03-28 16:55:34 +0700

    #code

---
+++ RedisExample/src/main/java/com/example/redisexample/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.example.redisexample.controller;
+
+import com.example.redisexample.entity.Product;
+import com.example.redisexample.repository.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductDao dao;
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return dao.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return dao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @Cacheable(key = "#id", value = "Product", unless = "#result.price > 1000")
+    public Product findProduct(@PathVariable int id) {
+        return dao.findProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @CacheEvict(key = "#id", value = "Product")
+    public String remove(@PathVariable int id) {
+        return dao.deleteProduct(id);
+    }
+}
